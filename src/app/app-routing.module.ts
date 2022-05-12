cae13d21f78ae70ad279e32724453d3c43062575
lqvinh1205 <77132358+lqvinh1205@users.noreply.github.com>
2022-05-12 07:24:03 +0700

    create project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelloComponent } from './pages/hello/hello.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'admin', component: HelloComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterAdminComponent } from './components/admin/footer-admin/footer-admin.component';
+import { HeaderAdminComponent } from './components/admin/header-admin/header-admin.component';
+import { AdminLayoutsComponent } from './pages/admin-layouts/admin-layouts.component';
+import { ClientLayoutsComponent } from './pages/client-layouts/client-layouts.component';
+import { HelloComponent } from './pages/hello/hello.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderAdminComponent,
+    FooterAdminComponent,
+    AdminLayoutsComponent,
+    ClientLayoutsComponent,
+    HelloComponent
   ],
   imports: [
     BrowserModule,
