1c5ae5b2b6462bb23f8e13e00b3b3e321a9d1650
lqvinh1205 <77132358+lqvinh1205@users.noreply.github.com>
2022-06-11 00:35:33 +0700

    fetch data from db to interface work-pages + fix some css

--- src/app/features/customer/work-pages/work-pages.component.ts
+++ src/app/features/customer/work-pages/work-pages.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as dayjs from 'dayjs';
+import { ProjectsService } from 'src/app/services/projects.service';
+import { ProjectElement } from 'src/app/shared/models/projects.model';
 
 @Component({
   selector: 'app-work-pages',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work-pages.component.css']
 })
 export class WorkPagesComponent implements OnInit {
-
-  constructor() { }
+  data!: ProjectElement[]
+  constructor(private ProjectsService: ProjectsService) { }
 
   ngOnInit(): void {
+      this.ProjectsService.getAllProject().subscribe((data) => {
+        data.map((item) => {
+          item.createAt = dayjs(item.createAt).format("YYYY")
+        })
+        this.data = data
+        console.log(this.data);
+        
+      })
   }
 
 }
